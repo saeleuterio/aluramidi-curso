824bebb365fb914d5acea882fd108eee1528b047
saeleuterio <saeleuterio@hotmail.com>
2024-11-01 17:42:56 -0300

    up

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-document.querySelector('.tecla_pom')
+// Pom -------------------------------
+function tocaSomPom () {
+    document.querySelector('#som_tecla_pom').play();
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+listaDeTeclas[0].onclick = tocaSomPom;
