bbbaa1d62a061be63c34926e574e96ca3c4cc5de
saeleuterio <saeleuterio@hotmail.com>
2024-11-04 20:44:21 -0300

    up

--- main.js
+++ main.js
@@ -15,4 +15,9 @@ for (let contador = 0; contador < listaDeTeclas.length; contador++) {
         tocaSom(idAudio);
     }
 
+    tecla.onkeydown = function () {
+        
+        tecla.classList.add('ativa');
+    }
+
 }
